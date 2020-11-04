526eb0f868cd41f8f2211963b85bbbfb96fd2ca8
ThomasMlh <storm62320@gmail.com>
2020-11-04 11:15:37 +0100

    Create api youtube call and a mock.json

---
+++ src/api/youtubeApiCall.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import mock from "./mock.json";
+
+export const getYoutube = async (query) => {
+  try {
+    const { data } = await axios.get(
+      `https://content.googleapis.com/youtube/v3/search?q=${query}&part=snippet&maxResults=1&type=video&key=AIzaSyD0qqnH0XQqwZDlHrZg35xpNNhKyoqD3Pk`
+    );
+    return data;
+  } catch (error) {
+    console.log(error);
+    return mock;
+  }
+};
