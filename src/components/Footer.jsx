05ef20af19bcec671647062f3fa3796d68349583
ThomasMlh <storm62320@gmail.com>
2020-11-06 11:06:54 +0100

    Some fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const Box = styled.div`
-  @media screen and (min-width: 450px) {
+  @media screen and (min-width: 500px) {
     height: 10vh;
     width: 100%;
     display: flex;
@@ -12,8 +12,8 @@ const Box = styled.div`
     border-top: 3px solid yellow;
   }
 
-  @media screen and (max-width: 450px) {
-    height: 20vh;
+  @media screen and (max-width: 500px) {
+    height: 10vh;
     width: 100%;
     display: flex;
     border-top: 3px solid yellow;
@@ -24,13 +24,13 @@ const DeskBox = styled.div`
   display: flex;
   width: 100%;
   justify-content: space-around;
-  @media screen and (max-width: 450px) {
+  @media screen and (max-width: 500px) {
     display: none;
   }
 `;
 
 const Lien = styled(Link)`
-  @media screen and (min-width: 450px) {
+  @media screen and (min-width: 500px) {
     font-size: 3vh;
     text-decoration: none;
     color: yellow;
@@ -38,23 +38,24 @@ const Lien = styled(Link)`
 `;
 
 const MobilBox = styled.div`
-  @media screen and (max-width: 450px) {
+  @media screen and (max-width: 500px) {
     width: 100%;
     display: flex;
     justify-content: space-around;
     align-items: center;
+    height: 100%;
   }
-  @media screen and (min-width: 450px) {
+  @media screen and (min-width: 500px) {
     display: none;
   }
 `;
 
 const LienMob = styled(Link)`
-  @media screen and (min-width: 450px) {
+  @media screen and (min-width: 500px) {
     display: none;
   }
-  @media screen and (max-width: 450px) {
-    font-size: 5vh;
+  @media screen and (max-width: 500px) {
+    font-size: 3vh;
     color: yellow;
   }
 `;
