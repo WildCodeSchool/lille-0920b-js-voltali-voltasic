fe14bbdc0ad8231b454c02247ca07a5154713311
Sarahshr <saraah.sahraoui@gmail.com>
2020-11-08 15:12:56 +0100

    add functions to get channels' pictures and trending songs

--- src/api/getTrend.js
+++ src/api/getTrend.js
@@ -4,7 +4,7 @@ import mock from "./mock.json";
 export const getTrend = async () => {
   try {
     const { data } = await axios.get(
-      `https://content.googleapis.com/youtube/v3/search?q=tendance+musique+fr&part=snippet&maxResults=1&type=video&key=AIzaSyD0qqnH0XQqwZDlHrZg35xpNNhKyoqD3Pk`
+      `https://content.googleapis.com/youtube/v3/search?q=musique+tendance+fr&part=snippet&maxResults=3&type=video&key=AIzaSyD0qqnH0XQqwZDlHrZg35xpNNhKyoqD3Pk`
     );
     return data;
   } catch (error) {
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import { getChannelPicture } from "../api/getChannelPicture";
 const Home = () => {
   const [items, setItems] = useState([]);
   const [channelPic, setChannelPic] = useState([]);
+  const [urlPictureChannel, setUrlPictureChannel] = useState({});
 
   const trend = async () => {
     const myItems = await getTrend();
@@ -11,8 +12,16 @@ const Home = () => {
     setItems(myItems.items);
   };
 
+  const channelPicture = async idChannel => {
+    const picture = await getChannelPicture(idChannel);
+    //console.log(picture.items[0].snippet.thumbnails.default.url);
+    setUrlPictureChannel(picture);
+  };
+
   useEffect(() => {
     trend();
+    items.map(item => setChannelPic(item.snippet.channelId));
+    channelPicture(channelPic);
   }, []);
 
   return (
@@ -31,10 +40,7 @@ const Home = () => {
       {items.map(item => (
         <div key={item.id.video}>
           <p>{item.snippet.channelTitle}</p>
-          <img
-            src={item.snippet.thumbnails.medium.url}
-            alt={item.snippet.title}
-          />
+          <img src="" alt={item.snippet.channelTitle} />
         </div>
       ))}
     </div>
