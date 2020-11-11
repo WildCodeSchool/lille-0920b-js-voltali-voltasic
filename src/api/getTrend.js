3fbb33b9688886c51a90676fa8170c5cb9466bfd
Sarahshr <saraah.sahraoui@gmail.com>
2020-11-11 01:15:03 +0100

    change color circles of carousel

--- src/api/getTrend.js
+++ src/api/getTrend.js
@@ -3,7 +3,7 @@ import mock from "./mock.json";
 export const getTrend = async () => {
   try {
     const { data } = await axios.get(
-      `https://content.googleapis.com/youtube/v3/search?q=musique+tendance+fr&part=snippet&maxResults=3&type=video&key=AIzaSyD0qqnH0XQqwZDlHrZg35xpNNhKyoqD3Pk`
+      `https://content.googleapis.com/youtube/v3/search?q=musique+tendance+fr&part=snippet&maxResults=5&type=video&key=AIzaSyD0qqnH0XQqwZDlHrZg35xpNNhKyoqD3Pk`
     );
     return data;
   } catch (error) {
