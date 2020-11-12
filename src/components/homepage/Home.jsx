e135a41169aa2b6bb367b42238dc24d8875ca91b
Fx-Colliot <fxcolliot@gmail.com>
2020-11-12 22:13:09 +0100

    added SweetMusics.jsx in homepage and component in Home.jsx

--- src/components/homepage/Home.jsx
+++ src/components/homepage/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TrendMusics from "./TrendMusics";
+import SweetMusics from "./Sweet";
 
 import "./Home.css";
 
@@ -7,6 +8,7 @@ const Home = () => {
   return (
     <>
       <TrendMusics />
+      <SweetMusics />
     </>
   );
 };
