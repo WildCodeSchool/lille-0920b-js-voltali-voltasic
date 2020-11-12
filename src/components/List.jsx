799427f3a3a01c4d6b31bd50121e2659d21c4d4a
ThomasMlh <storm62320@gmail.com>
2020-11-12 20:47:14 +0100

    List.jsx now using styled component !

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,57 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
-import "./List.css";
-
+import styled from "styled-components";
 import { getYoutube } from "../api/youtubeApiCall";
+
+const MainDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  padding-top: 3%;
+  flex-wrap: wrap;
+`;
+
+const MusicItem = styled.div`
+  display: flex;
+  align-items: center;
+  text-align: center;
+  color: yellow;
+  background: none;
+  transition: 0.5s ease;
+  margin-bottom: 3%;
+  @media (max-width: 1100px) {
+    width: 70%;
+  }
+  @media (max-width: 800px) {
+    width: 90%;
+    justify-content: flex-end;
+    padding-right: 0%;
+  }
+  @media (max-width: 650px) {
+    width: 90%;
+    flex-direction: column;
+    padding-right: 0%;
+  }
+  :hover {
+    transition: 0.5s ease;
+    transform: scale(1.1);
+  }
+`;
+
+const IMG = styled.img`
+  width: inherit;
+`;
+
+const Paragraph = styled.p`
+  width: 50%;
+`;
+
+const H1 = styled.h1`
+  color: white;
+  width: 50%;
+  text-align: start;
+`;
 
 const List = () => {
   const [items, setItems] = useState([]);
@@ -20,24 +69,25 @@ const List = () => {
     }
   }, [location.state.query]);
   return (
-    <div>
+    <MainDiv>
+      <H1>Results</H1>
       {items.map((item) => (
-        <div key={item.id.videoId}>
-          <p>{item.snippet.title}</p>
+        <MusicItem key={item.id.videoId}>
           <Link
             to={{
               pathname: `/music/${item.id.videoId}`,
               state: { title: item.snippet.title },
             }}
           >
-            <img
+            <IMG
               src={item.snippet.thumbnails.medium.url}
               alt={item.snippet.title}
             />
           </Link>
-        </div>
+          <Paragraph>{item.snippet.title}</Paragraph>
+        </MusicItem>
       ))}
-    </div>
+    </MainDiv>
   );
 };
 export default List;
