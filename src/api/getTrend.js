a723333d16dbf95333202b87839bcd2ecb6bbbe2
Sarahshr <saraah.sahraoui@gmail.com>
2020-11-12 01:15:07 +0100

    re rganisation of code of carousel

--- src/api/getTrend.js
+++ src/api/getTrend.js
@@ -1,9 +1,10 @@
 import axios from "axios";
+import Search from "../components/Search";
 import mock from "./mock.json";
-export const getTrend = async () => {
+export const getTrend = async searchValue => {
   try {
     const { data } = await axios.get(
-      `https://content.googleapis.com/youtube/v3/search?q=musique+tendance+fr&part=snippet&maxResults=5&type=video&key=AIzaSyBim8xQtVa1paCRELE98qW9xCCKml1NjTM`
+      `https://content.googleapis.com/youtube/v3/search?q=${searchValue}&part=snippet&maxResults=5&type=video&key=AIzaSyBD7jBT9SQik4EUky399mzp3BdVtlHCvJo`
     );
     return data;
   } catch (error) {
---
+++ src/components/homepage/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import TrendMusics from "./TrendMusics";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <TrendMusics />
+    </>
+  );
+};
+export default Home;
