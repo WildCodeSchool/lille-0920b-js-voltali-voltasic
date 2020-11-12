da8d6814b01a30a9c050dcce7b74f519315ce568
Sarahshr <saraah.sahraoui@gmail.com>
2020-11-12 09:50:55 +0100

    remove useless import

--- src/api/getTrend.js
+++ src/api/getTrend.js
@@ -1,10 +1,9 @@
 import axios from "axios";
-import Search from "../components/Search";
 import mock from "./mock.json";
 export const getTrend = async searchValue => {
   try {
     const { data } = await axios.get(
-      `https://content.googleapis.com/youtube/v3/search?q=${searchValue}&part=snippet&maxResults=5&type=video&key=AIzaSyBD7jBT9SQik4EUky399mzp3BdVtlHCvJo`
+      `https://content.googleapis.com/youtube/v3/search?q=${searchValue}&part=snippet&maxResults=5&type=video&key=AIzaSyBim8xQtVa1paCRELE98qW9xCCKml1NjTM`
     );
     return data;
   } catch (error) {
