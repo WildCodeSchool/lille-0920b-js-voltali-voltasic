472508e41482d4d43d631551b173e756249de829
ThomasMlh <storm62320@gmail.com>
2020-11-15 10:58:57 +0100

    Delete duplicate styled

--- src/components/homepage/MyCarousel.jsx
+++ src/components/homepage/MyCarousel.jsx
@@ -1,59 +1,18 @@
 import React, { useEffect, useState } from "react";
 import Carousel from "react-elastic-carousel";
-import { Link } from "react-router-dom";
+
+import {
+  Main,
+  Title,
+  SongTitles,
+  DivImg,
+  RouterLink,
+  Image,
+} from "./CarouselStyled";
 
 import { getYoutube } from "../../api/youtubeApiCall";
 
 import styled from "styled-components";
-
-const Main = styled.div`
-  padding: 1%;
-`;
-
-const Title = styled.h2`
-  font-size: 25pt;
-  color: white;
-`;
-
-const SongTitles = styled.p`
-  color: yellow;
-  font-size: 12pt;
-  text-shadow: black 0.1em 0.1em 0.2em;
-  text-align: center;
-  position: absolute;
-  background-color: rgba(255, 255, 255, 0);
-  visibility: hidden;
-  top: 45%;
-`;
-
-const Image = styled.img`
-  transition: transform 0.2s;
-  :hover {
-    transform: scale(1.1);
-  }
-`;
-
-const DivImg = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  position: relative;
-  :hover {
-    ${SongTitles} {
-      visibility: visible;
-      opacity: 1;
-    }
-    ${Image} {
-      transform: scale(1.1);
-    }
-  }
-`;
-
-const RouterLink = styled(Link)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-`;
 
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
