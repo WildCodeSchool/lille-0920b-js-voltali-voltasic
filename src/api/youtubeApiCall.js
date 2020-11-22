9092c6db95ddcb98b19fc0c9b4e8b6fab576cade
Sarahshr <saraah.sahraoui@gmail.com>
2020-11-22 15:18:48 +0100

    change key api

--- src/api/youtubeApiCall.js
+++ src/api/youtubeApiCall.js
@@ -4,7 +4,7 @@ import mock from "./mock.json";
 export const getYoutube = async query => {
   try {
     const { data } = await axios.get(
-      `https://content.googleapis.com/youtube/v3/search?q=${query}&part=snippet&maxResults=10&type=video&key=AIzaSyBim8xQtVa1paCRELE98qW9xCCKml1NjTM`
+      `https://content.googleapis.com/youtube/v3/search?q=${query}&part=snippet&maxResults=10&type=video&key=AIzaSyD0qqnH0XQqwZDlHrZg35xpNNhKyoqD3Pk`
     );
     return data;
   } catch (error) {
