2a6df13e870183efd13685668d854c1654029de1
Fx-Colliot <fxcolliot@gmail.com>
2020-11-24 12:40:08 +0100

    css player

--- src/components/Music.jsx
+++ src/components/Music.jsx
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import ReactPlayer from "react-player";
+import styled from "styled-components";
 
 import "./Music.css";
 const Music = ({ id }) => {
@@ -39,9 +40,48 @@ const Music = ({ id }) => {
       setPlayed(state.played);
     }
   };
+
+  const SongTitle = styled.h1`
+    font-size: 15pt;
+  `;
+
+  const Button = styled.button`
+    border: 1px solid black;
+    border-radius: 10px;
+    background-color: #1e1e20;
+    color: yellow;
+    width: 5vw;
+    height: 5vh;
+    :hover {
+      background-color: yellow;
+      color: #1e1e20;
+    }
+  `;
+  const LabelLoop = styled.label`
+    font-size: 2vh;
+    margin-left: 2%;
+    font-size: 3vh;
+  `;
+  const LabelVolume = styled.label`
+    font-size: 4vh;
+    margin-left: 8%;
+  `;
+  const Volume = styled.input`
+    height: 3vh;
+    margin-left: 7%;
+  `;
+
+  const AjustButton = styled.div`
+    display: flex;
+    align-items: center;
+    margin-right: 10%;
+    width: 50vw;
+    border: 1px solid red;
+  `;
+
   return (
-    <div>
-      <h1>{id.snippet.title}</h1>
+    <div className="lecteur">
+      <SongTitle>{id.snippet.title}</SongTitle>
       <ReactPlayer
         ref={inputRange}
         className="player"
@@ -51,41 +91,37 @@ const Music = ({ id }) => {
         playing={playing}
         loop={loop}
         onProgress={handleProgress}
+        height="0px"
       />
-      <button onClick={handlePlayPause}>{playing ? "Pause" : "Play"}</button>
-      <label htmlFor="volume">Volume</label>
-      <input
-        type="range"
-        min={0}
-        max={1}
-        value={volume}
-        step="any"
-        onChange={handleVolume}
-      />
-      <br />
-      <label htmlFor="loop">Loop</label>
-      <input
-        id="loop"
-        type="checkbox"
-        checked={loop}
-        onChange={handleToggleLoop}
-      />
-      <br />
-      Played
-      <progress max={1} value={played} />
-      <br />
-      Seek
-      <input
-        className="range_css"
-        type="range"
-        min={0}
-        max={0.999999}
-        step="any"
-        value={played}
-        onMouseDown={handleSeekMouseDown}
-        onChange={handleSeekChange}
-        onMouseUp={handleSeekMouseUp}
-      />
+      <AjustButton>
+        <Button className="play_button" onClick={handlePlayPause}>
+          {playing ? (
+            <i className="fas fa-pause"></i>
+          ) : (
+            <i className="fas fa-play"></i>
+          )}
+        </Button>
+        <LabelLoop htmlFor="loop">
+          <i className="fas fa-undo"></i>
+        </LabelLoop>
+        <input
+          id="loop"
+          type="checkbox"
+          checked={loop}
+          onChange={handleToggleLoop}
+        />
+        <LabelVolume htmlFor="volume">
+          <i className="fas fa-volume-up"></i>
+        </LabelVolume>
+        <Volume
+          type="range"
+          min={0}
+          max={1}
+          value={volume}
+          step="any"
+          onChange={handleVolume}
+        />
+      </AjustButton>
     </div>
   );
 };
